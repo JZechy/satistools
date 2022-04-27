f4b6cd7cf6a8f98970c14f7ff3ac0caa2583394c
Zechy <email@zechy.cz>
2022-04-27 14:12:48 +0200

    DbSets are nulled.

--- Satistools.GameData/GameDataContext.cs
+++ Satistools.GameData/GameDataContext.cs
@@ -7,11 +7,11 @@ namespace Satistools.GameData;
 
 public class GameDataContext : DbContext
 {
-    public DbSet<Item> Items { get; set; }
-    public DbSet<Recipe> Recipes { get; set; }
-    public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
-    public DbSet<RecipeProduct> RecipeProducts { get; set; }
-    public DbSet<BuildableManufacturer> BuildableManufacturers { get; set; }
+    public DbSet<Item> Items { get; set; } = null!;
+    public DbSet<Recipe> Recipes { get; set; } = null!;
+    public DbSet<RecipeIngredient> RecipeIngredients { get; set; } = null!;
+    public DbSet<RecipeProduct> RecipeProducts { get; set; } = null!;
+    public DbSet<BuildableManufacturer> BuildableManufacturers { get; set; } = null!;
 
     public GameDataContext(DbContextOptions<GameDataContext> options) : base(options)
     {
