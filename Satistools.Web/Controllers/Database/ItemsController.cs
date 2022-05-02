b8968f0bbc5dcfc9b227ceb1cf3ec36a12c7ced6
Zechy <email@zechy.cz>
2022-05-02 13:21:52 +0200

    DatabaseController split to Items & Recipes.

---
+++ Satistools.Web/Controllers/Database/ItemsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Satistools.GameData.Items;
+
+namespace Satistools.Web.Controllers.Database;
+
+/// <summary>
+/// Database of satisfactory items.
+/// </summary>
+[ApiController]
+[Route("database/[controller]")]
+public class ItemsController : ControllerBase
+{
+    private readonly IItemRepository _itemRepository;
+
+    public ItemsController(IItemRepository itemRepository)
+    {
+        _itemRepository = itemRepository;
+    }
+    
+    /// <summary>
+    /// Gets all parsed items from the FactoryGame in the alphabetical order.
+    /// </summary>
+    /// <returns>Enumeration of available items.</returns>
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Item>>> GetItems()
+    {
+        return Ok(await _itemRepository.FindAutomatedProducts());
+    }
+
+    /// <summary>
+    /// Gets information about single item by its ID.
+    /// </summary>
+    /// <param name="id">Identification of the item as the class name.</param>
+    /// <returns>Instance of found item or NotFound.</returns>
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<ActionResult<Item>> GetItem(string id)
+    {
+        Item? item = await _itemRepository.Get(id);
+        if (item is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(item);
+    }
+}
---
+++ Satistools.Web/Controllers/Database/RecipesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Satistools.GameData.Recipes;
+
+namespace Satistools.Web.Controllers.Database;
+
+/// <summary>
+/// Database of Satisfactory recipes.
+/// </summary>
+[ApiController]
+[Route("database/[controller]")]
+public class RecipesController : ControllerBase
+{
+    private readonly IRecipeRepository _recipeRepository;
+
+    public RecipesController(IRecipeRepository recipeRepository)
+    {
+        _recipeRepository = recipeRepository;
+    }
+
+    /// <summary>
+    /// Gets all recipes which are producing selected item.
+    /// </summary>
+    /// <param name="itemId">Identification of the item.</param>
+    /// <returns>Collection of recipes producing selected item with full data.</returns>
+    [HttpGet]
+    [Route("whoProduces/{itemId}")]
+    public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipesProducing(string itemId)
+    {
+        return Ok(await _recipeRepository.FindRecipesProducingItem(itemId));
+    }
+
+    /// <summary>
+    /// Gets all recipes which are using selecte item.
+    /// </summary>
+    /// <param name="itemId">Identification of item.</param>
+    /// <returns>Recipes which are using the selected items with full data.</returns>
+    [HttpGet]
+    [Route("whoUses/{itemId}")]
+    public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipesUsing(string itemId)
+    {
+        return Ok(await _recipeRepository.FindRecipesUsingItem(itemId));
+    }
+}
